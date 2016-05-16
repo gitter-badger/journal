a3a3d7285eaf63435f37d9d5c685b8693fc14e96
Paul Campbell <paulcampbell@fife.ac.uk>
2016-05-16 14:30:27 +0100

    service.JournalService.getLogs(LocalDate): add method

--- src/main/java/net/kemitix/journal/service/JournalService.java
+++ src/main/java/net/kemitix/journal/service/JournalService.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import net.kemitix.journal.model.DailyLog;
+import net.kemitix.journal.model.LogEntry;
 
 /**
  * Interface for managing a Journal.
@@ -21,7 +22,17 @@ public interface JournalService {
 
     /**
      * Returns a list of all the DailyLogs.
+     *
      * @return a list of DailyLogs
      */
     List<DailyLog> getAllDailyLogs();
+
+    /**
+     * Returns a list of all the LogEntries for the given date.
+     *
+     * @param date the date
+     *
+     * @return the list of log entries
+     */
+    List<LogEntry> getLogs(LocalDate date);
 }
