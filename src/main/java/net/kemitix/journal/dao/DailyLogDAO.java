78536de35ce7a1ac24f94e919d97ce2008bdcdf6
Paul Campbell <paulcampbell@fife.ac.uk>
2016-05-16 15:06:23 +0100

    dao.DailyLogDAO.findByDate(LocalDate): added

--- src/main/java/net/kemitix/journal/dao/DailyLogDAO.java
+++ src/main/java/net/kemitix/journal/dao/DailyLogDAO.java
@@ -1,16 +1,25 @@
 package net.kemitix.journal.dao;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.kemitix.journal.model.DailyLog;
 
 /**
- * DAO for {@link net.kemitix.journal.model.DailyLog}.
+ * DAO for {@link DailyLog}.
  *
  * @author pcampbell
  */
 public interface DailyLogDAO extends JpaRepository<DailyLog, LocalDate> {
 
+    /**
+     * Find the Daily Log for the given date.
+     *
+     * @param date the date
+     *
+     * @return the daily log
+     */
+    Optional<DailyLog> findByDate(LocalDate date);
 }
