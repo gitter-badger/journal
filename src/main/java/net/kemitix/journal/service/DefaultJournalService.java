c09ccc9dae15341abb4515570896b69ab7235fe3
Paul Campbell <paulcampbell@fife.ac.uk>
2016-05-16 14:30:50 +0100

    service.DefaultJournalService.getLogs(LocalDate): added

--- src/main/java/net/kemitix/journal/service/DefaultJournalService.java
+++ src/main/java/net/kemitix/journal/service/DefaultJournalService.java
@@ -3,14 +3,18 @@ package net.kemitix.journal.service;
 import lombok.val;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
 import net.kemitix.journal.dao.DailyLogDAO;
+import net.kemitix.journal.dao.LogEntryDAO;
 import net.kemitix.journal.model.DailyLog;
+import net.kemitix.journal.model.LogEntry;
 
 /**
  * Default implementation of the JournalService.
@@ -22,9 +26,13 @@ class DefaultJournalService implements JournalService {
 
     private final DailyLogDAO dailyLogDAO;
 
+    private final LogEntryDAO logEntryDAO;
+
     @Inject
-    DefaultJournalService(final DailyLogDAO dailyLogDAO) {
+    DefaultJournalService(
+            final DailyLogDAO dailyLogDAO, final LogEntryDAO logEntryDAO) {
         this.dailyLogDAO = dailyLogDAO;
+        this.logEntryDAO = logEntryDAO;
     }
 
     @Override
@@ -37,5 +45,14 @@ class DefaultJournalService implements JournalService {
     @Override
     public List<DailyLog> getAllDailyLogs() {
         return dailyLogDAO.findAll();
+    }
+
+    @Override
+    public List<LogEntry> getLogs(final LocalDate date) {
+        final Optional<DailyLog> optional = dailyLogDAO.findByDate(date);
+        if (optional.isPresent()) {
+            return optional.get().getEntries();
+        }
+        return new ArrayList<>();
     }
 }
