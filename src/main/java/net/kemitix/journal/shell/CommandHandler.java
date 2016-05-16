f64da7bf2ed373671471475bba055c563f877849
Paul Campbell <paulcampbell@fife.ac.uk>
2016-05-16 12:30:19 +0100

    shell.CommandHandler: add missing @return javadoc tags

--- src/main/java/net/kemitix/journal/shell/CommandHandler.java
+++ src/main/java/net/kemitix/journal/shell/CommandHandler.java
@@ -13,6 +13,8 @@ interface CommandHandler {
 
     /**
      * Returns a list of aliases for the command. May be multiple words.
+     *
+     * @return list of command aliases
      */
     List<String> getAliases();
 
@@ -20,6 +22,8 @@ interface CommandHandler {
      * Returns an Optional containing a regex Pattern to match against
      * parameters that follow the matched alias. If the command does not take
      * any parameters then the Optional will be empty.
+     *
+     * @return an Optional containing any parameter regex
      */
     Optional<String> getParameterRegex();
 
@@ -33,11 +37,15 @@ interface CommandHandler {
 
     /**
      * Returns the syntax of the command for display in help messages.
+     *
+     * @return the syntax
      */
     String getSyntax();
 
     /**
      * Returns the description of the command.
+     *
+     * @return the description
      */
     String getDescription();
 
