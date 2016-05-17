27c33f4e4525ec3b14750163f730d3a90c466f70
Paul Campbell <paulcampbell@fife.ac.uk>
2016-05-17 09:57:11 +0100

    shell.CommandPromptTest: test main loop

--- src/test/java/net/kemitix/journal/shell/CommandPromptTest.java
+++ src/test/java/net/kemitix/journal/shell/CommandPromptTest.java
@@ -1,8 +1,8 @@
 package net.kemitix.journal.shell;
 
+import lombok.val;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -14,8 +14,10 @@ import static org.mockito.Mockito.verify;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Optional;
 
+import net.kemitix.journal.TypeSafeHashMap;
 import net.kemitix.journal.TypeSafeMap;
 
 /**
@@ -25,13 +27,11 @@ import net.kemitix.journal.TypeSafeMap;
  */
 public class CommandPromptTest {
 
-    @InjectMocks
     private CommandPrompt prompt;
 
     @Mock
     private CommandRouter commandRouter;
 
-    @Mock
     private TypeSafeMap applicationState;
 
     @Mock
@@ -40,20 +40,75 @@ public class CommandPromptTest {
     @Mock
     private PrintWriter output;
 
+    @Mock
+    private CommandMapping commandMapping;
+
+    @Mock
+    private CommandHandler commandHandler;
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+        applicationState = new TypeSafeHashMap();
+        prompt = new CommandPrompt(commandRouter, applicationState, reader,
+                output);
     }
 
     @Test
     public void shouldAbortOnIOException() throws IOException {
         //given
         doThrow(IOException.class).when(reader).readLine();
-        given(applicationState.get("running", Boolean.class)).willReturn(
-                Optional.of(Boolean.TRUE));
         //when
         prompt.run();
         verify(output).println(startsWith("Aborting: "));
+    }
+
+    @Test
+    public void shouldExitWhenInputIsNull() throws IOException {
+        //given
+        given(reader.readLine()).willReturn(null);
+        //when
+        prompt.run();
+        //then
+        verify(output).println("Exiting...");
+    }
+
+    @Test
+    public void shouldLoopWhenInputIsEmpty() throws IOException {
+        //given
+        given(reader.readLine()).willReturn("").willReturn(null);
+        //when
+        prompt.run();
+        //then
+        verify(output).println("Exiting...");
+    }
+
+    @Test
+    public void shouldDispatchRecognisedCommand() throws IOException {
+        //given
+        given(reader.readLine()).willReturn("command").willReturn(null);
+        given(commandRouter.selectCommand("command")).willReturn(
+                Optional.of(commandMapping));
+        given(commandMapping.getHandler()).willReturn(commandHandler);
+        val args = new HashMap<String, String>();
+        given(commandMapping.getArgs()).willReturn(args);
+        given(commandHandler.handle(args)).willReturn("expected output");
+        //when
+        prompt.run();
+        //then
+        verify(output).println("expected output");
+    }
+
+    @Test
+    public void shouldReportUnknownCommand() throws IOException {
+        //given
+        given(reader.readLine()).willReturn("command").willReturn(null);
+        given(commandRouter.selectCommand("command")).willReturn(
+                Optional.empty());
+        //when
+        prompt.run();
+        //then
+        verify(output).println("Not a recognised command!");
     }
 
 }
